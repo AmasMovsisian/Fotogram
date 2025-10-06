b2d9499fc07c5a2123dacace9cb33f08d74ebb11
AmasMovsisian <232450988+AmasMovsisian@users.noreply.github.com>
2025-10-06 17:57:23 +0200

    Impressum updated. Also added extra impressum styles for css

---
+++ scripts.js
@@ -0,0 +1,21 @@
+let myArray = [img_1.jpg,
+,img_10.jpg
+,img_11.jpg
+,img_12.jpg
+,img_13.jpg
+,img_14.jpg
+,img_15.png
+,img_2.jpg
+,img_3.jpg
+,img_4.jpg
+,img_5.jpg
+,img_6.jpg
+,img_7.jpg
+,img_8.jpg
+,img_9.jpg]
+
+const dialogRef = document.getElementById("myImagesDialog");
+
+function openDialog() {
+    dialogRef.showModal();
+}
